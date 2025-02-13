40fc721f53045a007823df165317775fee3813a8
sandykr29 <sandeepkumar29774@yahoo.com>
2025-02-13 12:32:06 +0530

    middleware updated

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.header('Authorization')?.split(' ')[1];
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
